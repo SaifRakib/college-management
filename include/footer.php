3bd9580d01b02bbad9a8b131320913d43bbe5aa0
Saif Rakib Hasan <saifrakib99@gmail.com>
2021-07-24 16:17:02 +0600

    College management

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<footer class="">
+<div class="container text-center">
+  <p class="font-italic mb-0">&copy; Copyrights Rakib-2021 All rights reserved.</p>
+</div>
+</footer>
---
+++ photo.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style-photo.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="text-center mb-4 mt-3"><b>Photo Gallery</b></h2>
+        </div>
+      </div>
+        <!-- Gallery -->
+<div class="row">
+    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <img
+        src="images/student1.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt=""
+      />
+  
+      <img
+        src="images/student2.jpeg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt=""
+      />
+    </div>
+  
+    <div class="col-lg-4 mb-4 mb-lg-0">
+      <img
+        src="images/student2.jpeg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt=""
+      />
+  
+      <img
+        src="images/student1.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt=""
+      />
+    </div>
+  
+    <div class="col-lg-4 mb-4 mb-lg-0">
+      <img
+        src="images/student1.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt=""
+      />
+  
+      <img
+        src="images/student2.jpeg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        alt=""
+      />
+    </div>
+  </div>
+  <!-- Gallery -->
+    </div>
+</body>
+</html>
